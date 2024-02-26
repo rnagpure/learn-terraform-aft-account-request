94434c72199c8c1b43755dfcab923077f15bab64
Rahul Nagpure <rahulnagpure@Rahuls-MacBook-Pro.local>
2024-02-26 22:09:59 +1100

    ADD UAT

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,3 +25,31 @@ module "Development" {
 
   account_customizations_name = "Dev"
 }
+
+module "UAT" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "rahulnagpure+uat@gmail.com"
+    AccountName               = "UAT"
+    ManagedOrganizationalUnit = "UAT"
+    SSOUserEmail              = "rahulnagpure+uat@gmail.com"
+    SSOUserFirstName          = "UAT"
+    SSOUserLastName           = "UAT"
+  }
+
+  account_tags = {
+    "Managed by" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Rahul Nagpure"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "UAT"
+}
